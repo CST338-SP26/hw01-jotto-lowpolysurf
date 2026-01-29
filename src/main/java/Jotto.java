cf90e643fb0ae85a4514794835dc3a8d825f6165
lowpolysurf <lowpolysurf@gmail.com>
2026-01-29 10:53:04 -0800

    init

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,3 +1,110 @@
+import java.util.ArrayList;
+
+public class Jotto(){
+
+    private final int WORD_SIZE;
+    private String currentWord = "";
+    private int score;
+    ArrayList<String> playGuesses = new ArrayList<String>();
+    ArrayList<String> playWords = new ArrayList<String>();
+    private string filename;
+    ArrayList<String> wordList = new ArrayList<String>();
+    private final boolean DEBUG;
+
+    public Jotto(String filename){
+
+    }
+
+    public boolean pickWord(){
+
+    }
+    public String showWordList(){
+
+    }
+    public ArrayList<String> showPlayerGuesses(){
+
+    }
+    public void playerGuessScores(ArrayList<String>){
+
+    }
+    public void setCurrentWord(String word){
+
+    }
+    public ArrayList<String> readWords(){
+
+    }
+    public void play(){
+
+    }
+    public int guess(){
+
+    }
+    public ArrayList<String> getPlayedWords() {
+
+    }
+    public String getCurrentWord(){
+
+    }
+    public int getLetterCount(String word){
+
+    }
+    public String showPlayedWords(){
+
+    }
+    public boolean addPlayerGuess(String word){
+
+    }
+    public void updateWordList(){
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
  * @author feng3302
  * @version 0.1.0
